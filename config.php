517398f282584b8b90942c4ababa68dbfb613151
dddnoid <34587143+dddnoid@users.noreply.github.com>
2017-12-25 16:24:28 +0700

    menambahkan database dan fungsi input registrasi user

--- config.php
+++ config.php
@@ -10,5 +10,5 @@ $db = mysqli_connect($server, $user, $password, $nama_database);
 if( !$db ){
     die("Gagal terhubung dengan database: " . mysqli_connect_error());
 }
-
+echo "Berhasil Terhubung";
 ?>
