5f9f78fcb027ac67fe98bc01b1532a1380e97c49
dddnoid <34587143+dddnoid@users.noreply.github.com>
2017-12-30 18:41:10 +0700

    Semua beres tinggal dicoba

---
+++ front-end/views/dashboard.php
@@ -0,0 +1,15 @@
+<div class="row">
+          <div class="col-lg-12">
+            <h1>Dashboard<small></small></h1>
+            <ol class="breadcrumb">
+              <li><a href="index.html"><i class="icon-dashboard"></i>Dashboard</a></li>
+            </ol>
+          </div>
+        </div>
+		<div class="row">
+			<div class="col-lg-12">
+				Selamat Datang di Portal Resmi Gov-Talk
+				Sebagai sarana penerimaan aspirasi dan pengaduan masyarakat, laporan warga yang masuk via 
+				Gov-Talk dikelola dan didisposisikan ke berbagai instansi terkait yang terhubung untuk direspons langsung.
+			</div>
+		</div>
---
+++ front-end/views/layanan.php
@@ -0,0 +1,35 @@
+		<div class="row">
+          <div class="col-lg-13">
+            <h1>Daftar Keluhan</h1>
+            <ol class="breadcrumb">          
+			  <button class="btn btn-info btn-xs"><i class= "fa fa-edit"></i> Tambah</button>
+            </ol>
+          </div>
+        </div>
+		<div class="row">
+			<div class="col-lg-13">
+				<div class ="table responsive">
+					<table class = "table table-bordered table-hover table-stripped">
+						<tr>	
+							<th>No</th>
+							<th>Id User</th>
+							<th>Topik</th>
+							<th>Keluhan</th>
+							<th>Opsi</th>
+						</tr>
+						
+						<tr>
+							<td>1</td>
+							<td>rafidan08</td>
+							<td>Banjir</td>
+							<td>Banjir yang melanda bojongsoang dan sekitarnya tolong segera dibenahi pak Kamil </td>
+							<td align = "center">
+								
+								<button class="btn btn-success btn-xs"><i class= "fa fa-edit"></i> Edit</button>
+								<button class="btn btn-danger btn-xs"><i class= "fa fa-edit"></i> Hapus</button>
+							</td>
+						</tr>
+					</table>	
+			</div>
+		</div>
+	</div>
